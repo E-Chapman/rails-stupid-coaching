00c8e0f5ed3299fd20a412b2017d62f86576e12f
Elizabeth Chapman <elizabethmaychapman@gmail.com>
2022-07-14 20:25:21 +0100

    Stupid coaching challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,15 @@
 class QuestionsController < ApplicationController
-  def ask 
+  def ask
   end
 
   def answer
     @question = params[:ask]
-    if @question ==  "I am going to work"
-        @answer = "Great!"
-    else @question ?
-    @answer = 'Silly question, get dressed and go to work!' : @answer = "I don't care, get dressed and go to work!"
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.strip.ends_with?("?")
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I do not care, get dressed and go to work!'
     end
+  end
 end
